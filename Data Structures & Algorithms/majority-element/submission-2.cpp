02797761ec296691ae40962e1fee7be9fcb109d9
Jaya Prakash <jayaprakashreddydharmana@gmail.com>
2026-04-09 23:40:43 +0530

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size()/2;
+        unordered_map<int,int> freq;
+        for (int i=0;i<nums.size();i++){
+            freq[nums[i]]++;
+            if (freq[nums[i]]>n) return nums[i];
+        }
+        return -1;
+    }
+};
